c79a4951c68631293263ee68f8c2136a4b78dc74
Alfredo <surfalfie@gmail.com>
2026-06-04 08:31:17 -0700

    Admin work

---
+++ HW1 aafont/src/guiRemoveUser/java/modelRemoveUser.java
@@ -0,0 +1,20 @@
+package guiRemoveUser.java;
+
+/*******
+ * <p> Title: ModelDeleteUser Class. </p>
+ * 
+ * <p> Description: The DeleteUser Page Model.  This class is not used as there is no
+ * data manipulated by this MVC beyond selecting a user and removing that user from the
+ * database.</p>
+ * 
+ * 
+ * 
+ * @author Alfredo Afont
+ * 
+ * @version 1.00		2025-08-15 Initial version
+ *  
+ */
+
+public class modelRemoveUser {
+
+}
