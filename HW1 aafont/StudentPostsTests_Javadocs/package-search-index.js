f8bcf9089d1d497327ed78a64d5619ca109a6c1a
molldonn3 <122385186+molldonn3@users.noreply.github.com>
2026-06-29 23:05:15 -0400

    Delete HW1 aafont/StudentPostsTests_Javadocs directory

--- HW1 aafont/StudentPostsTests_Javadocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"},{"m":"FoundationsF26","l":"testClasses"}];updateSearchResults();
--- HW1 aafont/StudentPostsTests_Javadocs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html","k":"18"},{"p":"testClasses","l":"StudentPostManualTests"},{"p":"testClasses","l":"StudentPostSemiAutomatedTests"}];updateSearchResults();
